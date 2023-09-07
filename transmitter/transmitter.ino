351135f9913dbaf8a3cb80f25594e6e5c884566b
goranssh <132730822+goranssh@users.noreply.github.com>
2023-09-07 16:20:23 +0530

    transmitter.ino

---
+++ transmitter/transmitter.ino
@@ -0,0 +1,106 @@
+#include <ArduinoMqttClient.h>
+#if defined(ARDUINO_SAMD_MKRWIFI1010)  defined(ARDUINO_SAMD_NANO_33_IOT)  defined(ARDUINO_AVR_UNO_WIFI_REV2)
+#include <WiFiNINA.h>
+#elif defined(ARDUINO_SAMD_MKR1000)
+#include <WiFi101.h>
+#include <WiFi.h>
+#endif
+
+char ssid[] = Rahul ;  
+char pass[] = Rahul123;    
+
+const int trigPin = 2;
+const int echoPin = 3;
+
+float duration, distance;
+
+WiFiClient wifiClient;
+MqttClient mqttClient(wifiClient);
+
+const char broker[] = "broker.mqttdashboard.com";
+int        port     = 1883;
+const char topic[]  = "SIT210/waves";
+
+const long interval = 1000;
+unsigned long previousMillis = 0;
+
+int count = 0;
+
+void setup() {
+  //Initialize serial and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  // attempt to connect to WiFi network:
+  Serial.print("Attempting to connect to WPA SSID: ");
+  Serial.println(ssid);
+  while (WiFi.begin(ssid, pass) != WL_CONNECTED) {
+    // failed, retry
+    Serial.print(".");
+    delay(5000);
+  }
+
+  Serial.println("You're connected to the network");
+  Serial.println();
+
+  // You can provide a unique client ID, if not set the library uses Arduino-millis()
+  // Each client must have a unique client ID
+  // mqttClient.setId("clientId");
+
+  // You can provide a username and password for authentication
+  // mqttClient.setUsernamePassword("username", "password");
+
+  Serial.print("Attempting to connect to the MQTT broker: ");
+  Serial.println(broker);
+
+  if (!mqttClient.connect(broker, port)) {
+    Serial.print("MQTT connection failed! Error code = ");
+    Serial.println(mqttClient.connectError());
+
+    while (1);
+  }
+
+  Serial.println("You're connected to the MQTT broker!");
+  Serial.println();
+}
+
+void loop() {
+
+  mqttClient.poll();
+
+
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= interval)
+  {
+
+    previousMillis = currentMillis;
+
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+
+    duration = pulseIn(echoPin, HIGH);
+    distance = (duration * .0343) / 2;
+    Serial.print("Distance: ");
+    Serial.println(distance);
+    if (distance < 12)
+    {
+     
+      mqttClient.beginMessage(topic);
+      mqttClient.print("Sohil Nagpal : Wave is detected, ");
+      mqttClient.print("Distance: ");
+      mqttClient.print(distance);
+      mqttClient.endMessage();
+      delay(1000);
+    }
+
+    Serial.println();
+
+    count++;
+  }
+}
